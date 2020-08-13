9594bfb9f9cde41f086b14dcbbb9909599096f7b
James Wilson <jgwilson1214@gmail.com>
2020-08-12 23:21:39 -0700

    refactor closed form

--- problemset/1180A.cpp
+++ problemset/1180A.cpp
@@ -4,15 +4,10 @@ int main() {
     int n, ans = 0;
     cin >> n;
 
-    if (n == 1) {
-        ans = 1;
-    }
-    else {
-        for (int i = 0; i + 1 < n; i++) {
-            ans += 2 * (2 * i + 1);
-        }
-        ans += 2 * (n - 1) + 1;
-    }
+    // sum of odds => n^2
+    // nth odd => 2n+1
+    ans = 2 * (n * n) - 2 * n + 1;
+
     cout << ans << endl;
 
     return 0;
