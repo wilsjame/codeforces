1088519f76d04e57a30a70abdc5b13012255b715
James Wilson <jgwilson1214@gmail.com>
2020-08-17 00:39:59 -0700

    refactor: sum of two odds is even

--- problemset/1323A.cpp
+++ problemset/1323A.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
 int main() {
     int t;
     cin >> t;
-
     while (t--) {
         int n;
         cin >> n;
         int a[n];
+        // sum of two odds is even approach
+        bool found = false;
         for (int i = 0; i < n; i++) {
             cin >> a[i];
+            if (a[i] % 2 == 0 && !found) {
+                cout << 1 << endl << i + 1 << endl;
+                found = true;
+            }
         }
+        if (!found) {
+            if (n == 1) {
+                cout << -1 << endl;
+            }
+            else {
+                cout << 2 << endl << "1 2" << endl;
+            }
+        }
+    }
+    return 0;
+}
 
+/*
         bool found = false;
         vector<int> b = {};
         for (int i = 0; i < n && !found; i++) {
@@ -37,7 +52,4 @@ int main() {
             cout << -1;
         }
         cout << endl;
-
-    }
-    return 0;
-}
+        */
