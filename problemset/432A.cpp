3f61e8f77dedf9cdc19621d572c35b0524e70602
James Wilson <jgwilson1214@gmail.com>
2020-08-19 12:51:57 -0700

    refactor

--- problemset/432A.cpp
+++ problemset/432A.cpp
@@ -3,12 +3,23 @@
 #include <algorithm>
 using namespace std;
 int main() {
-    int n, k, ans = 0;;
+    int n, k, ans, cnt = 0;
     cin >> n >> k;
     vector<int> a(n);
+
+    // O(n)
     for (int i = 0; i < n; i++) {
         cin >> a[i];
+        if (a[i] + k <= 5) {
+           cnt++; 
+        }
     }
+
+    ans = cnt / 3;
+    cout << ans << endl;
+
+    /*
+    O(nlogn) + O(n)
 
     sort(a.begin(), a.end());
     if (n >= 3) {
@@ -19,7 +30,7 @@ int main() {
         }
     }
     cout << ans << endl;
-
+    */
 
     return 0;
 }
