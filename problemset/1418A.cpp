9429ab4c4477db0cd38aaea3d945191c7e626435
wilsjame <jgwilson1214@gmail.com>
2020-09-17 01:32:29 -0700

    buying torches

--- problemset/1418A.cpp
+++ problemset/1418A.cpp
@@ -4,21 +4,24 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        long long x, y, k, s = 1;
+        long long x, y, k;
         cin >> x >> y >> k;
-        long long trades = 0;
-        // need k coals
-        // k * y sticks
-        // need k sticks
-        // (k * y) + k sticks
-        while (s < y * k + k) {
-            s += x;
-            s--;
-            cerr << s << " ";
-            trades++;
-        }
-        cout << trades;
-
+        // need to perform second trade k times to get enough coals
+        // so trades += k
+        // now how many total sticks do we need?
+        // need k coals and k sticks
+        // ky sticks for coals
+        // k sticks additionally to pair with the coals 
+        // so ky + k sticks total
+        // we initially have one stick!
+        // so we actually need ky + k - 1 sticks
+        // how many trades to get this many sticks
+        // x - 1 stick per trade
+        // ceiling (total / trade amount)
+        // ceil(ky + k - 1 / x - 1)
+        // ceil(a/b) = floor(a + b - 1 / b)
+        // ky + k + x - 1 - 1 - 1 / x - 1
+        cout << k + ((k*y+k+x-3)/(x-1)) << endl;
     }
     return 0;
 }
