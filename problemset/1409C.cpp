6c7247375707e93ac1a0cd644c33dd307c1d36db
wilsjame <jgwilson1214@gmail.com>
2020-10-09 17:56:04 -0700

    cleanup code: yet another array restoration

--- problemset/1409C.cpp
+++ problemset/1409C.cpp
@@ -7,45 +7,39 @@ int main() {
     cin >> T;
     while (T--) {
         int n, X, Y;
-        cin >> n >> X >> Y;
-        // X < Y
+        cin >> n >> X >> Y; // X < Y
 
-        vector<int> best(n, 1e9 + 5);
-        vector<int> a(n, 1e9 + 5);
+        int diff = Y - X;
+        vector<int> a(n), best(n, 1e9 + 5);
+
         // brute force all positions of x and y for sequence size n: O(n^2)
         // for each x and y position fill the arithmetic sequence: O(n)
         // => O(n^3)
-        int diff = Y - X;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 // partitions are the spaces between values in the sequence
                 // a,b has one partition, a,b,c has two partitions
                 int partitions = j - i;
+
                 if (diff % partitions == 0) {
-                    int partition_size = diff / partitions;
+                    int idx, partition_size = diff / partitions;
                     a[j] = Y;
+
                     // build sequence to the right
-                    int value = Y, idx = j + 1;
+                    idx = j + 1;
                     while (idx < n) {
-                        a[idx] = value + partition_size;
-                        value += partition_size;
+                        a[idx] = a[idx - 1] + partition_size;
                         idx++;
                     }
                     // build sequence to the left
-                    value = Y, idx = j - 1;
+                    idx = j - 1;
                     while (idx >=0) {
-                        if (value - partition_size <= 0) {
-                            // invalid!
-                            a[n-1] = 1e9 + 5; // kinda clean? xD
-                            break;
-                        }
-                        a[idx] = value - partition_size;
-                        value -= partition_size;
+                        a[idx] = a[idx + 1] - partition_size;
                         idx--;
                     }
+
                     assert(a[i] == X);
-                    // is the last element less than best's last element?
-                    if (a[n - 1] < best[n - 1]) {
+                    if (a[0] > 0 && a[n - 1] < best[n - 1]) {
                         best = a;
                     }
                 }
