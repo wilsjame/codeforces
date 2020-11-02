8c28dbb743d478945dff84e7cc8a0c11da662d73
wilsjame <jgwilson1214@gmail.com>
2020-11-02 15:36:17 -0800

    refactor usinf Diophantine properties

--- problemset/787A.cpp
+++ problemset/787A.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    while (b != d && b < 1e5) {
+    int ans = -1;
+    for (int i = 0; i <= max({a, b, c, d,}) && ans < 0; i++) {
+        for (int j = 0; j <= max({a, b, c, d}) && ans < 0; j++) {
+            if (a * i + b == c * j + d) {
+                ans = a * i + b; // then break;
+            }
+        }
+    }
+    cout << ans;
+
+    /*
+    while (b != d && b < a * c) {
         if (b < d)
             b += a;
         else
@@ -12,6 +24,7 @@ int main() {
     }
     //cerr << "b " << b << " d " << d << endl;
     cout << (b==d ? b : -1);
+    */
 
     return 0;
 }
